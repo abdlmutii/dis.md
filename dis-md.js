035f95e94679ed890da26c1e6782c60243a56f90
Seif <92731667+abdlmutii@users.noreply.github.com>
2023-02-28 18:28:26 +0200

    Update dis-md.js

--- dis-md.js
+++ dis-md.js
@@ -1,21 +1,43 @@
 function parse(text) {
+
   const replacements = [
+
     { pattern: /\*\*(.*?)\*\*/g, replacement: '<strong>$1</strong>' },
+
     { pattern: /__(.*?)__/g, replacement: '<u>$1</u>' },
+
     { pattern: /~~(.*?)~~/g, replacement: '<s>$1</s>' },
-    { pattern: /`(.*?)`/g, replacement: '<span style="background-color: black; font-family: monospace; color: white;">$1</span>' },
+
+    { pattern: /`([^`]+)`/g, replacement: '<code>$1</code>' },
+
+    { pattern: /\[(.*?)\]\((.*?)\)/g, replacement: '<a style="color:#00AAFC " href="$2">$1</a>' },
+
+    { pattern: /(https?:\/\/[^\s]+)/g, replacement: '<a style="color: #00AAFC" href="$1">$1</a>' },
+
     { pattern: /\n/g, replacement: '<br />' },
-    { pattern: /(^|\s)`{3}([^\r\n]+)\r?\n([\s\S]+?)\r?\n`{3}($|\s)/gm, replacement: '<pre>$2\n$3</pre>' },
-    { pattern: /(^|\s)`(.+?)`($|\s)/gm, replacement: '<code>$2</code>' },
+
+    { pattern: /(^|\s)```([a-z]+)?\n([\s\S]+?)\n```($|\s)/gm, replacement: '<pre>$3</pre>' },
+
     { pattern: /(_)([^_]+)(_)/g, replacement: '<em>$2</em>' },
+
     { pattern: /(\*\*)([^*]+)(\*\*)/g, replacement: '<strong>$2</strong>' },
+
+    { pattern: /(\*)([^*]+)(\*)/g, replacement: '<em>$2</em>' },
+
   ];
 
   replacements.forEach(({ pattern, replacement }) => {
+
     text = text.replace(pattern, replacement);
+
   });
 
   return text;
+
 }
 
-module.exports = { parse }
+if (typeof module !== 'undefined') {
+
+  module.exports = { parse };
+
+}
